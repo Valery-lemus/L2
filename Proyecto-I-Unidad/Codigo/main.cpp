7964a46ac421d37e1e1e68f61e5266d0e027fb4c
Valery-lemus <valery.lemus@unah.hn>
2022-06-21 00:52:20 -0600

    Agregando Proyecto I Unidad

---
+++ Proyecto-I-Unidad/Codigo/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "menu.h"
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    system("cls");
+
+    // starShip();
+    // snake();
+
+    menu();
+
+    return 0;
+}
